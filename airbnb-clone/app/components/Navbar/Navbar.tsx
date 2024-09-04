f8fd5b38324436aef3e6216c32a0b5b96425115b
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-09-04 19:14:33 -0300

    feat: add initial Search component

--- airbnb-clone/app/components/Navbar/Navbar.tsx
+++ airbnb-clone/app/components/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import Container from "../Container"
+import Logo from "./Logo"
+import Search from "./Search"
 
 const Navbar = () => {
     return (
@@ -6,7 +8,9 @@ const Navbar = () => {
             <div className="py-4 border-b-[1px]">
                 <Container>
                     <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-                        <h1>loggo</h1>
+                        <Logo />
+
+                        <Search />
                     </div>
                 </Container>
             </div>
